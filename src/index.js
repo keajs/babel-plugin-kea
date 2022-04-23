9a53ff91fbdaaaf9a2fa6f431bc4df70bc99cdcd
Marius Andra <marius.andra@gmail.com>
2022-04-23 23:07:08 +0200

    support path() builders

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 const nodePath = require('path')
 
-export default function ({ types: t, template }) {
+export default function ({ types: t }) {
   function isKeaCall(node) {
-    return t.isIdentifier(node.callee, { name: 'kea' }) &&
+    return (
+      t.isIdentifier(node.callee, { name: 'kea' }) &&
       node.arguments.length === 1 &&
-      t.isObjectExpression(node.arguments[0])
+      (t.isObjectExpression(node.arguments[0]) || t.isArrayExpression(node.arguments[0]))
+    )
   }
 
-  function buildPathProperty(node, pathParts) {
-    const hasKey = node.arguments[0].properties.find(property => t.isIdentifier(property.key, { name: 'key' }))
-    const builder = hasKey
-      ? template('const a = { path: (key) => ' + (JSON.stringify(pathParts).replace(/\]$/, ', key]')) + ' }')
-      : template('const a = { path: () => ' + JSON.stringify(pathParts) + ' }')
-    const builtNodes = builder()
-
-    return builtNodes.declarations[0].init.properties[0]
-  }
-
-  function getLocalPath (state) {
+  function getLocalPath(state) {
     let root = state.file.opts.root
     if (state.opts.path) {
       root = nodePath.resolve(root, state.opts.path)
@@ -25,35 +17,46 @@ export default function ({ types: t, template }) {
     return nodePath.relative(root, state.file.opts.filename)
   }
 
-  function hasPathProperty (path) {
-    return path.node.arguments[0].properties.find(property => t.isIdentifier(property.key, { name: 'path' }))
-  }
-
   const KeaVisitor = {
     CallExpression(path) {
-      if (!isKeaCall(path.node) || hasPathProperty(path)) {
+      if (!isKeaCall(path.node)) {
         return
       }
-
       this.counter += 1
-
       let pathParts = this.pathParts
-
       if (this.counter > 1) {
-        pathParts = this.pathParts.map(p => p) // make a copy
+        pathParts = this.pathParts.map((p) => p) // make a copy
         pathParts[pathParts.length - 1] = pathParts[pathParts.length - 1] + '/' + this.counter
       }
-
-      const pathProperty = buildPathProperty(path.node, pathParts)
-
-      path.node.arguments[0].properties.push(pathProperty)
-    }
+      if (t.isObjectExpression(path.node.arguments[0])) {
+        if (!path.node.arguments[0].properties.find((property) => t.isIdentifier(property.key, { name: 'path' }))) {
+          const pathProperty = t.objectProperty(
+            t.identifier('path'),
+            t.arrayExpression(pathParts.map((str) => t.stringLiteral(str))),
+          )
+          path.node.arguments[0].properties = [pathProperty, ...path.node.arguments[0].properties]
+        }
+      } else if (t.isArrayExpression(path.node.arguments[0])) {
+        if (
+          !path.node.arguments[0].elements.find(
+            (element) => t.isCallExpression(element) && t.isIdentifier(element.callee, { name: 'path' }),
+          )
+        ) {
+          const pathBuilder = t.callExpression(t.identifier('path'), [
+            t.arrayExpression(pathParts.map((str) => t.stringLiteral(str))),
+          ])
+          path.node.arguments[0].elements = [pathBuilder, ...path.node.arguments[0].elements]
+        }
+      }
+    },
   }
 
   return {
     visitor: {
       Program(path, state) {
-        const pathParts = getLocalPath(state).replace(/\.(js|jsx|ts|tsx)$/, '').split(nodePath.sep)
+        const pathParts = getLocalPath(state)
+          .replace(/\.(js|jsx|ts|tsx)$/, '')
+          .split(nodePath.sep)
 
         if (pathParts[0] === '..') {
           return
@@ -61,23 +64,27 @@ export default function ({ types: t, template }) {
 
         path.traverse(KeaVisitor, {
           pathParts: pathParts.map(toCamelCase),
-          counter: 0
-        });
+          counter: 0,
+        })
       },
-    }
+    },
   }
 }
 
 function toCamelCase(str) {
   // Lower cases the string
-  return str
-    // Replaces any - or _ characters with a space
-    .replace( /[-_]+/g, ' ')
-    // Removes any non alphanumeric characters
-    .replace( /[^\w\s]/g, '')
-    // Uppercases the first character in each group immediately following a space
-    // (delimited by spaces)
-    .replace( / (.)/g, function($1) { return $1.toUpperCase(); })
-    // Removes spaces
-    .replace( / /g, '' );
+  return (
+    str
+      // Replaces any - or _ characters with a space
+      .replace(/[-_]+/g, ' ')
+      // Removes any non alphanumeric characters
+      .replace(/[^\w\s]/g, '')
+      // Uppercases the first character in each group immediately following a space
+      // (delimited by spaces)
+      .replace(/ (.)/g, function ($1) {
+        return $1.toUpperCase()
+      })
+      // Removes spaces
+      .replace(/ /g, '')
+  )
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,43 @@
-/* global test */
-import { transform } from "@babel/core";
-import keaPlugin from "./index";
+/* global test, describe, expect */
+import { transform } from '@babel/core'
+import keaPlugin from './index'
 
-test("it works", () => {
-  const oldCode = `
-  const logic = kea({
-    actions: { doit: true, }
+describe('babel-plugin-kea', () => {
+  test('works with kea 2.0 logic input', () => {
+    const oldCode = `
+      const logic = kea({
+        actions: { doit: true, }
+      })
+    `
+
+    const { code } = transform(oldCode, {
+      envName: 'production',
+      code: true,
+      babelrc: false,
+      configFile: false,
+      filename: 'scenes/page/index.ts',
+      plugins: [keaPlugin],
+    })
+
+    expect(code).toMatchSnapshot()
   })
-  `;
 
-  const { code } = transform(oldCode, {
-    envName: "production",
-    code: true,
-    babelrc: false,
-    configFile: false,
-    filename: "scenes/page/index.ts",
-    // presets: [["env", { targets: { node: process.versions.node } }]],
-    plugins: [keaPlugin],
-  });
-  expect(code).toMatchSnapshot();
-});
+  test('works with kea 3.0 logic builders', () => {
+    const oldCode = `
+      const logic = kea([
+        actions({ doit: true }),
+      ])
+    `
+
+    const { code } = transform(oldCode, {
+      envName: 'production',
+      code: true,
+      babelrc: false,
+      configFile: false,
+      filename: 'scenes/page/index.ts',
+      plugins: [keaPlugin],
+    })
+
+    expect(code).toMatchSnapshot()
+  })
+})
